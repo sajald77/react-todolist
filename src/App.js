67a620cce06aac6fa8d280fa83f774b0ebad27fa
Sajal <sazzaldulal@gmail.com>
2018-07-24 13:44:48 +0545

    redux connected

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
-import AddTodo from './components/addTodo';
 
-const App = () => (
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import AddTodo from "./components/addTodo";
+import actions from "./actions";
+
+export const App = ({ submitTodo }) => (
   <div>
     <h1>Todo list</h1>
-    <AddTodo submitTodo={() => {}} />
+    <AddTodo submitTodo={submitTodo} />
   </div>
 );
 
-export default App;
+App.propTypes = {
+  submitTodo: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = state => state.todoListApp;
+
+const mapDispatchToProps = dispatch => ({
+  submitTodo: text => {
+    if (text) {
+      dispatch(actions.submitTodo(text));
+    }
+  },
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(App);
